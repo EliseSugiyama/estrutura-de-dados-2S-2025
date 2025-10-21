ee43baa0f7d255f1d86a6f5bc4fcfc5c8008922b
EliseSugiyama <elise.sugiyama5@gmail.com>
2025-10-21 12:51:28 -0300

    Initial commit

---
+++ aula10/Exemplo01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Item{
+    
+    int Chave;
+    struct Item * Proximo; 
+
+} Item;
+
+int main() {
+    
+    Item * I = (Item*) malloc (sizeof(Item));
+    
+    I->Chave = 23;
+    I->Proximo = NULL;
+
+    printf("endereco %x\n", I);
+    printf("%d\n", I->Chave);
+    printf("%x (NULL)\n", I->Proximo); 
+}
