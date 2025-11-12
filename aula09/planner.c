806c141f4c06a876f2a051d00836a8d1c3a2023c
EliseSugiyama <elise.sugiyama5@gmail.com>
2025-11-12 01:46:48 -0300

    initial commit

---
+++ aula09/planner.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "planner.h"
+
+Lista * CriarLista (int C){
+    Lista * L = (Lista *)malloc(sizeof(Lista));
+
+    if(L == NULL){
+        fprintf(stderr, "ERRO: Não há memória para a estrutura da lista!");
+        return NULL;
+
+    }
+
+    L -> Dados = (Tarefa*)malloc(C * sizeof(Tarefa));
+
+    if(L -> Dados == NULL){
+        printf("ERRO: Não há memória para o vetor de tarefas!");
+        free(L);
+        return NULL;
+    }
+
+    L -> Tamanho = 0;
+    L -> Capacidade = C;
+    
+    return L;
+}
+
+void DestruirLista(Lista * L){
+    if(L == NULL) return;
+    free(L -> Dados);
+    free(L);
+}
+
+void AdicionarTarefa(Lista * L, Tarefa T){
+    if (L == NULL){
+        printf("ERRO: lista inválida!\n");
+        return;
+    }   
+    
+    if(L -> Tamanho == L -> Capacidade){
+        printf("ERRO: lista cheia!\n");
+        return;
+    }
+
+    L -> Dados[L -> Tamanho] = T;
+    L -> Tamanho++;
+}
